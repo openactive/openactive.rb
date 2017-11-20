f9a75477a6ff6b5a9b2a44f1c93e5bd067796e95
Roryg <giannichan@gmail.com>
2017-11-20 11:54:07 +0000

    build datasets from new directory json url and format

--- lib/openactive/datasets.rb
+++ lib/openactive/datasets.rb
@@ -1,22 +1,19 @@
 module OpenActive
   class Datasets
 
-    FORKS="https://api.github.com/repos/openactive/dataset-site-generator/forks?sort=stargazers"
+    DIRECTORY="https://www.openactive.io/datasets/directory.json"
 
     def self.list
-      resp = RestClient.get( FORKS )
-      forks = JSON.parse(resp.body)
+      resp = RestClient.get( DIRECTORY )
+      results = JSON.parse(resp.body)
       datasets = []
-      forks.each do |fork|
+      results.each do |result|
         begin
-          next unless fork["stargazers_count"] >= 1
-          resp = RestClient.get("https://raw.githubusercontent.com/#{fork['full_name']}/master/metadata.json")
-          metadata = JSON.parse(resp.body)
-          datasets << {
-              id: fork['name'],
-              title: metadata["title"],
-              data_url: metadata["data-url"]
-          } if metadata["publish"] && metadata["publish"] == true
+          next unless result["publish"] && result["publish"] == true
+          result.merge!({
+            "id" => result["documentation-url"].gsub("https://github.com/", "")
+          })
+          datasets << result
         rescue => e
           #ignore errors
         end
