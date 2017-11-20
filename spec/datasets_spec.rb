e46437ffdff030f2489faf6f7a6c635dcbea2e36
Roryg <giannichan@gmail.com>
2017-11-20 11:38:04 +0000

    change spec and fixtures to new datasets metadata json

--- spec/datasets_spec.rb
+++ spec/datasets_spec.rb
@@ -3,19 +3,17 @@ require 'spec_helper'
 describe OpenActive::Datasets do
 
   before(:each) do
-    WebMock.stub_request(:get, "https://api.github.com/repos/openactive/dataset-site-generator/forks?sort=stargazers").to_return(body: load_fixture("forks.json"))
-    WebMock.stub_request(:get, "https://raw.githubusercontent.com/good-gym/goodgym-oa.github.io/master/metadata.json").to_return(body: load_fixture("metadata.json"))
-    WebMock.stub_request(:get, "https://raw.githubusercontent.com/acwoodcock/dataset-site-generator/master/metadata.json").to_return(body: load_fixture("metadata-not-published.json"))
+    WebMock.stub_request(:get, "https://www.openactive.io/datasets/directory.json").to_return(body: load_fixture("directory.json"))
   end
 
   it "should extract name and url" do
     datasets = OpenActive::Datasets.list
-    expect( datasets.first ).to eql({ id: "goodgym-oa.github.io", title: "GoodGym Sessions", data_url: "https://www.goodgym.org/getHappenings"})
+    expect( datasets.first ).to eql({ id: "activenewham/opendata", title: "activeNewham Sessions", data_url: "https://activenewham-openactive.herokuapp.com"})
   end
 
   it "should extract all published datasets" do
     datasets = OpenActive::Datasets.list
-    expect( datasets.length ).to eql(1)
+    expect( datasets.length ).to eql(2)
   end
 
 end
