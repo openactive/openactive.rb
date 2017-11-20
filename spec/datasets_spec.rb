a239d5e7c8ccf8fd8a23358734d6211f21ce72e7
Roryg <giannichan@gmail.com>
2017-11-20 11:53:47 +0000

    compare to full fixture result item

--- spec/datasets_spec.rb
+++ spec/datasets_spec.rb
@@ -8,7 +8,33 @@ describe OpenActive::Datasets do
 
   it "should extract name and url" do
     datasets = OpenActive::Datasets.list
-    expect( datasets.first ).to eql({ id: "activenewham/opendata", title: "activeNewham Sessions", data_url: "https://activenewham-openactive.herokuapp.com"})
+    expect( datasets.first ).to eql({
+
+      "id" => "activenewham/opendata",
+      "dataset-site-url" => "http://data.activenewham.org.uk/",
+      "title" => "activeNewham Sessions",
+      "description" => "Session data from the activenewham.org.uk site",
+      "publisher-name" => "activeNewham",
+      "publisher-url" => "https://www.activenewham.org.uk",
+      "keyword-1" => "Newham",
+      "keyword-2" => "Leisure",
+      "data-url" => "https://activenewham-openactive.herokuapp.com",
+      "created" => "2017-02-17",
+      "documentation-url" => "https://github.com/activenewham/opendata",
+      "rpde-version" => "0.2.3",
+      "license-name" => "Creative Commons Attribution Licence (CC-BY v4.0)",
+      "license-url" => "https://creativecommons.org/licenses/by/4.0/",
+      "attribution-text" => "activeNewham",
+      "attribution-url" => "http://data.activenewham.org.uk/",
+      "mailchimp" => "require([\"mojo/signup-forms/Loader\"], function(L) { L.start({\"baseUrl\":\"mc.us15.list-manage.com\",\"uuid\":\"139e08e5077eeef9048052685\",\"lid\":\"5eb5e5184a\"}) })",
+      "copyright-notice" => "",
+      "odi-certificate-number" => "",
+      "publish" => true,
+      "example-url" => [
+       "https://imin-ltd.github.io/gladstone-adapter-documentation/example.json"
+      ]
+      
+    })
   end
 
   it "should extract all published datasets" do
