faafce53ac2b9ed92f6c79d1e93591e48bea6e33
Roryg <giannichan@gmail.com>
2017-12-07 09:44:23 +0000

    move github and conformance calls out of gem

--- lib/openactive/datasets.rb
+++ lib/openactive/datasets.rb
@@ -10,31 +10,8 @@ module OpenActive
       results.each do |result|
         begin
           next unless result["publish"] && result["publish"] == true
-
           dataset_key = result["documentation-url"].gsub("https://github.com/", "")
           dataset_key.chomp!("/")
-
-          begin
-            feed = OpenActive::Feed.new(result["data-url"])
-            page = feed.fetch
-            result.merge!({
-              "uses-opportunity-model" => page.declares_oa_context?.eql?(true),
-              "uses-paging-spec" => page.valid_rpde?
-            })
-          rescue
-            #ignore errors
-          end
-
-          begin
-            git_resp = RestClient.get('https://api.github.com/repos/'+ dataset_key +'/issues')
-            issues = JSON.parse(git_resp.body)
-            result.merge!({
-              "github-issues" => issues.size
-            })
-          rescue
-            #ignore errors
-          end
-
           datasets.merge!({ dataset_key => result })
         rescue => e
           #ignore errors
--- spec/datasets_spec.rb
+++ spec/datasets_spec.rb
@@ -4,10 +4,6 @@ describe OpenActive::Datasets do
 
   before(:each) do
     WebMock.stub_request(:get, "https://www.openactive.io/datasets/directory.json").to_return(body: load_fixture("directory.json"))
-    WebMock.stub_request(:get, "https://activenewham-openactive.herokuapp.com/").to_return(status: 200, body: load_fixture("single-item.json"))
-    WebMock.stub_request(:get, "https://makesweat.com/service/openactive.php").to_return(status: 200, body: load_fixture("single-item.json"))
-    WebMock.stub_request(:get, "https://api.github.com/repos/activenewham/opendata/issues").to_return(status: 200, body: "[{}]", headers: {})
-    WebMock.stub_request(:get, "https://api.github.com/repos/makesweat/opendata/issues").to_return(status: 200, body: "[{}]", headers: {})
   end
 
   it "should extract dataset meta from directory json" do
@@ -30,41 +26,12 @@ describe OpenActive::Datasets do
       "attribution-url" => "http://data.activenewham.org.uk/",
       "mailchimp" => "require([\"mojo/signup-forms/Loader\"], function(L) { L.start({\"baseUrl\":\"mc.us15.list-manage.com\",\"uuid\":\"139e08e5077eeef9048052685\",\"lid\":\"5eb5e5184a\"}) })",
       "copyright-notice" => "",
-      "github-issues" => 1,
       "odi-certificate-number" => "",
       "publish" => true,
-      "uses-opportunity-model" => true,
-      "uses-paging-spec" => true,
       "example-url" => [
        "https://imin-ltd.github.io/gladstone-adapter-documentation/example.json"
       ]
     })
-  end
-
-  it "should extract dataset meta even if it couldn't page the data" do
-    WebMock.stub_request(:get, "https://activenewham-openactive.herokuapp.com/").to_return(status: 500)
-    datasets = OpenActive::Datasets.list
-    expect( datasets["activenewham/opendata"] ).to include(
-      "dataset-site-url", "title", "description", "publisher-name", "publisher-url", 
-      "keyword-1", "keyword-2", "data-url", "created", "documentation-url", 
-      "rpde-version", "license-name", "license-url", "attribution-text", 
-      "attribution-url", "mailchimp", "github-issues", "example-url"
-    )
-    expect( datasets["activenewham/opendata"] ).not_to include("uses-opportunity-model", "uses-paging-spec")
-  end
-
-  it "should extract dataset meta even if it couldn't reach github api" do
-    WebMock.stub_request(:get, "https://api.github.com/repos/activenewham/opendata/issues").to_return(status: 500)
-    datasets = OpenActive::Datasets.list
-    
-    expect( datasets["activenewham/opendata"] ).to include(
-      "dataset-site-url", "title", "description", "publisher-name", "publisher-url", 
-      "keyword-1", "keyword-2", "data-url", "created", "documentation-url", 
-      "rpde-version", "license-name", "license-url", "attribution-text", 
-      "attribution-url", "mailchimp", "uses-opportunity-model", "uses-paging-spec", 
-      "example-url")
-
-    expect( datasets["activenewham/opendata"] ).not_to include("github-issues")
   end
 
   it "should extract all published datasets" do
